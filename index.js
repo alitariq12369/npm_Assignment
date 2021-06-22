974f398e778e0919735486961451cde0466fa20e
alitariq12369 <alitariq.uet2020@gmail.com>
2021-06-22 21:49:20 +0500

    Half Assignment ---Displayed Colors---

---
+++ index.js
@@ -0,0 +1,7 @@
+//Installed the package and inserted it here
+const chalk = require('chalk');
+
+// All the three colors are displayed
+console.log(chalk.green('This is green color text'))
+console.log(chalk.red('This is red color text'))
+console.log(chalk.blue('This is blue color text'))
